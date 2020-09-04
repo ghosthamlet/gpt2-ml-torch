b7761e20af5700c6673a54a083cbf36f1d1611bd
gvvvv <gvvvv@163.com>
2020-09-05 01:47:48 +0800

    add clue token model

--- gpt2_ml_torch/config.py
+++ gpt2_ml_torch/config.py
@@ -3,3 +3,7 @@
 MODEL_PATH = './models/mega-bert-tok/'
 MODEL_TF = './models/mega-bert-tok-tf/model.ckpt-100000'
 CONFIG_TF = './models/mega-bert-tok-tf/mega.json'
+
+# MODEL_PATH = './models/mega-clue-tok/'
+# MODEL_TF = './models/mega-clue-tok-tf/model.ckpt-220000'
+# CONFIG_TF = './models/mega-clue-tok-tf/mega.json'
